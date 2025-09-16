22f6bc9472bd7ef2629746bc1b4e919f282cc5bc
vj-2303 <vanaraj1018@gmail.com>
2025-09-17 00:10:55 +0530

    Create a logical directory structure for our project.
    Set up a basic web server that can listen for and respond to HTTP
    requests.
    
    Create a healthcheck endpoint to verify that our server is running
    correctly.

---
+++ cmd/api/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
+func main() {
+
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "devolopment", "Environment (development|staging|production)")
+
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	go func() {
+		logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+		err := srv.ListenAndServe()
+		if err != nil {
+			logger.Error(err.Error())
+			os.Exit(1)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+	logger.Info("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	logger.Info("server stopped")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vj-2303/voting-api-go
+
+go 1.25.0
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
